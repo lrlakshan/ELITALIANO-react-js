6f8bcf92ad9b935a94267ed673e606c1d33bf702
Lakshan Rathnayaka <lakshan@omobio.net>
2019-01-18 17:48:42 +0530

    UI::Old UI deleted

--- Application/my_app/src/config/Config.js
+++
@@ -1,5 +0,0 @@
-const Config = {
-    baseUrl: 'http://appsuite.robi.com.bd/robi_appsuite/staging/bizlogic/web/index.php?r='
-};
-
-export default Config;
--- Application/my_app/src/utils/Helper.js
+++
@@ -1,8 +0,0 @@
-import Config from "./../config/Config";
-import Http from "./Http";
-
-const Helper = {
-    http: new Http(Config.baseUrl)
-}
-
-export default Helper;
