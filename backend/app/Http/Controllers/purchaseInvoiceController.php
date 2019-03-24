799cf7f393b8c60b17a045d416ff5c9089da33c3
Lakshan Rathnayaka <lakshan@omobio.net>
2019-03-24 21:22:11 +0530

    Biz Logic::return to supplier get purchase invoice data

--- backend/app/Http/Controllers/purchaseInvoiceController.php
+++ backend/app/Http/Controllers/purchaseInvoiceController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\purchase_invoice;
 use Validator;
 use Exception;
 
 class purchaseInvoiceController extends Controller
 {
-    //
+    //add purchase invoice record
     public function addPurchaseInvoice(Request $request){
 
         try {
@@ -40,6 +41,7 @@ class purchaseInvoiceController extends Controller
         }
     }
 
+    //get the next invoice number for purchase
     public function purchaseInvoiceNextNumber(){
     	try {
     	$next_invoice_number = array();
@@ -59,5 +61,36 @@ class purchaseInvoiceController extends Controller
     			'code'=>500
     		], 500);
     	}
+    }
+
+    //get all invoices list
+    public function getInvoiceDetails(){
+        try {
+        $invoiceDetials = array();
+        $invoiceDetials = DB::table('purchase_invoices')
+                            ->join('supplier_details', 'purchase_invoices.supplierId', '=', 'supplier_details.id')
+                            ->select(
+                                'purchase_invoices.invoiceNum',
+                                'supplier_details.supplierName', 
+                                'purchase_invoices.date', 
+                                'purchase_invoices.details',
+                                'purchase_invoices.totalBill'
+                            )
+                            ->paginate(5);
+            return response()->json([
+                'success'=>true,
+                'error'=>null,
+                'code'=>200,
+                'total'=>count($invoiceDetials),
+                'data'=>$invoiceDetials
+            ], 200);
+            
+        } catch (Exception $e) {
+            return response()->json([
+                'success'=>false,
+                'error'=>($e->getMessage()),
+                'code'=>500
+            ], 500);
+        }
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -43,4 +43,5 @@ Route::get('supplierDetails','supplierDetailController@supplierDetails');
 
 //api's from purchase_invoices table
 Route::get('purchaseInvoiceNextNumber','purchaseInvoiceController@purchaseInvoiceNextNumber');
+Route::get('getInvoiceDetails','purchaseInvoiceController@getInvoiceDetails');
 Route::post('addPurchaseInvoice','purchaseInvoiceController@addPurchaseInvoice');
