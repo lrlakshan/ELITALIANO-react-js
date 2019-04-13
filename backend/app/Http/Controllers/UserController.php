57bce46b71add28a14deac9815ce863381e57e80
Lakshan Rathnayaka <lakshan@omobio.net>
2019-04-13 12:31:21 +0530

    Biz Logic::Login page backend validations

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -14,24 +14,33 @@ class UserController extends Controller
 {
     //
     public function userLogin(Request $request){
-    	$validator = Validator::make($request->all(), [
-    		'username'=>'required',
-    		'password'=>'required'
-    	]);
 
-    	if($validator->fails()){
-    		return response()->json(['success'=>false,'error'=>$validator->errors(),'code'=>401]);
-    	}
+        try {
+            $validator = Validator::make($request->all(), [
+            'username'=>'required',
+            'password'=>'required'
+        ]);
 
-    	if(Auth::attempt(['username'=>request('username'), 'password'=>request('password')])){
-    		$user = Auth::user();
-    		// $success['token'] = $user->createToken('MyApp')->accessToken;
-    		return response()->json(['success'=>true,'error'=>$validator->errors(),'code'=>200,'data'=>$user], 200);
-    	}
+        if($validator->fails()){
+            return response()->json(['success'=>false,'error'=>$validator->errors(),'code'=>401]);
+        }
 
-    	else{
-    		return response()->json(['success'=>false,'error'=>'unauthorized','code'=>200]);
-    	}
+        if(Auth::attempt(['username'=>request('username'), 'password'=>request('password')])){
+            $user = Auth::user();
+            // $success['token'] = $user->createToken('MyApp')->accessToken;
+            return response()->json(['success'=>true,'error'=>$validator->errors(),'code'=>200,'data'=>$user], 200);
+        }
+
+        else{
+            return response()->json(['success'=>false,'error'=>'unauthorized','code'=>400]);
+        }
+        } catch (Exception $e) {
+            return response()->json([
+                'success'=>false,
+                'error'=>($e->getCode()),
+                'code'=>500
+            ], 500);
+        }
     }
 
     public function userRegister(Request $request){
